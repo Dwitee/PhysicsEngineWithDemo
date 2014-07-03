e5132eba98652cb58ea4a8bad678d27a1b3596e2
Dwitee <dwitee@gmail.com>
2014-07-03 17:36:02 +0530

    Update timing.h

--- mechaEngine-physics-master/src/demos/timing.h
+++ mechaEngine-physics-master/src/demos/timing.h
@@ -2,13 +2,6 @@
  * Timing functions.
  *
  * Part of the mechaEngine physics system.
- *
- * Copyright (c) Ian Millington 2003-2006. All Rights Reserved.
- *
- * This software is distributed under licence. Use of this software
- * implies agreement with all terms and conditions of the accompanying
- * software licence.
- */
 
 /**
  * @file
